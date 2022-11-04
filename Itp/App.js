60c12c140f322ab7eff061089e09a664d4728074
esamperio593 <esamperio593@gamil.com>
2022-11-03 21:41:49 -0600

    ultimo comit

--- Itp/App.js
+++ Itp/App.js
@@ -2,21 +2,38 @@ import React, {useEffect, useState} from 'react';
 import {View, ActivityIndicator, Text} from 'react-native';
 import {Image, Button} from '@rneui/themed';
 import styles from './src/styles/styles';
-import img1 from './src/assets/img1.jpg'
+import img1 from './src/assets/img1.jpeg';
+import img2 from './src/assets/img2.jpeg';
+import img3 from './src/assets/img3.jpeg';
+import img4 from './src/assets/img4.jpeg';
+import {getText} from './src/services/getData';
+
+const arrayImg = [img1, img2, img3, img4];
 
 const App = () => {
+  const [txt, setTxt] = useState ('valor pr dflt');
+
+  useEffect(() => {
+    updateTxt();
+  }, []);
+
+  const updateTxt = () => getText().then(result => setTxt(result.fact));
+
+  const getImg = () => arrayImg[Math.floor(Math.random() * arrayImg.length)];
+
   return (
     <View style={styles.main}>
       <View style={styles.left}>
-      <Button title="Actualizar" />
+      <Button 
+      title="Actualizar" onPress={updateTxt} />
       </View>
       <Image
-        source={img1}
+        source={getImg}
         containerStyle={styles.item}
         PlaceholderContent={<ActivityIndicator />}
       />
       <Text style = {styles.txt}> 
-        kdhjdhjjk ihigiuhiou 
+      {txt}
       </Text>
   </View>
   );
